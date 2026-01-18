2399ca957fea07d4030bbf30224dfbe78fbd0554
raees-aziz <raees4963@gmail.com>
2026-01-18 23:02:41 +0500

    featured componenet

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -11,13 +11,27 @@ function Feature() {
                     {/* left card */}
                     <div className='cardcontainer w-1/2 h-[75vh]'>
                         <div className='card w-full h-full rounded-xl overflow-hidden bg-green-500'>
-                            <img className='size-full bg-cover' src="/public/image/project-1.png" alt="" />
+                            <img className='size-full bg-cover' src="image/project-1.png" alt="" />
                         </div>
                     </div>
                     {/* right card */}
                     <div className='cardcontainer w-1/2 h-[75vh]'>
                         <div className='card w-full h-full rounded-xl overflow-hidden bg-green-500'>
-                            <img className='size-full bg-cover' src="/public/image/project-2.png" alt="" />
+                            <img className='size-full bg-cover' src="image/project-2.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className='cards w-full flex gap-10 mt-10'>
+                    {/* left card */}
+                    <div className='cardcontainer w-1/2 h-[75vh]'>
+                        <div className='card w-full h-full rounded-xl overflow-hidden bg-green-500'>
+                            <img className='size-full bg-cover' src="image/project-1.png" alt="" />
+                        </div>
+                    </div>
+                    {/* right card */}
+                    <div className='cardcontainer w-1/2 h-[75vh]'>
+                        <div className='card w-full h-full rounded-xl overflow-hidden bg-green-500'>
+                            <img className='size-full bg-cover' src="image/project-2.png" alt="" />
                         </div>
                     </div>
                 </div>
