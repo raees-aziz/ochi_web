e6f07f4998fa031dabd5e02b6789172a20523b42
raees-aziz <raees4963@gmail.com>
2026-01-23 01:48:50 +0500

    animation section

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import { FaArrowUpLong } from "react-icons/fa6";
 function LandingPage() {
  
     return (
-        <div className="w-full h-screen bg-zinc-900 pt-1">
+        <div data-scroll data-scroll-section data-scroll-speed='-.1' className="w-full h-screen bg-zinc-900 pt-1">
             <div className="textstructure mt-52 px-20">
                 {["We Create", "Eye Opening", "Presentation"].map((item,index) => {
                     return (
