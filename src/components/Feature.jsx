0d4f2c7da1475bf924949c58c352340e4859083b
raees-aziz <raees4963@gmail.com>
2026-01-25 15:41:08 +0500

    fixing github error

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Feature() {
     return (
         <div className='w-full py-10 mb-20'>
-            <div className='w-full px-20 border-b-[1px] border-zinc-500 pb-20'>
+            <div className='w-full px-20 border-b border-zinc-500 pb-20'>
                 <h1 className='text-8xl font-["Neue_Montreal"] tracking-tight '>Featured Project</h1>
             </div>
             <div className='px-20'>
@@ -14,16 +14,16 @@ function Feature() {
                       {"RAEES".split('').map((item,index)=> <span className='inline-block' key={index}>{item}</span>)}
                         </h1>
                             <div className='card w-full h-full rounded-xl overflow-hidden bg-green-500'>
-                                <img className='size-full bg-cover' src="/public/image/project-1.png" alt="" />
+                                <img className='size-full bg-cover' src="/image/project-1.png" alt="" />
                             </div>
                         </div>
                         {/* right card */}
                         <div className='cardcontainer relative w-1/2 h-[75vh]'>
                          <h1 className='absolute right-full translate-x-1/2 -translate-y-1/2 top-1/2 text-7xl leading-none tracking-tighter font-bold text-[#cdea68] '>
-                      {"VICE".split('').map((item,index)=> <span key={index}>{item}</span>)}
+                      {"VICE".split('').map((item,index)=> <span className='inline-block' key={index}>{item}</span>)}
                         </h1>
                             <div className='card w-full h-full rounded-xl overflow-hidden bg-green-500'>
-                                <img className='size-full bg-cover' src="/public/image/project-2.png" alt="" />
+                                <img className='size-full bg-cover' src="/image/project-2.png" alt="" />
                             </div>
                         </div>
                     </div>
